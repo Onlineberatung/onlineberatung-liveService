2017c3cbacc13d7de22564aed5a78c6b1fc36e94
mebo4b <eberl@open4business.de>
2020-10-07 16:02:27 +0200

    chore: provide required deployment scripts

---
+++ src/main/java/de/caritas/cob/liveservice/LiveServiceApplication.java
@@ -0,0 +1,13 @@
+package de.caritas.cob.liveservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LiveServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/de/caritas/cob/liveservice/LiveServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.caritas.cob.liveservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
