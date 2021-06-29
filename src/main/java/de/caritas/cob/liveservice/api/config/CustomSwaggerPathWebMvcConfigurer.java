be9deae183bf7562ba8a5f7860b19a6a608994df
Markus Eberl <eberl@open4business.de>
2021-06-29 09:01:30 +0200

    chore: add cors mapping

--- src/main/java/de/caritas/cob/liveservice/api/config/CustomSwaggerPathWebMvcConfigurer.java
+++ src/main/java/de/caritas/cob/liveservice/api/config/CustomSwaggerPathWebMvcConfigurer.java
@@ -2,6 +2,7 @@ package de.caritas.cob.liveservice.api.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,4 +18,11 @@ public class CustomSwaggerPathWebMvcConfigurer implements WebMvcConfigurer {
         .addResourceLocations("classpath:/META-INF/resources/");
   }
 
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("*")
+        .allowCredentials(true);
+  }
 }
